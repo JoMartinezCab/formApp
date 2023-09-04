d2922e848ec6f5949af7e428d72bd72a5eea7448
Jorge Martinez <jo.cabmartin2@gmail.com>
2023-09-04 16:21:50 -0500

    Sistema de enrutamiento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'reactive',
+  loadChildren: () => import('./modules/reactive/reactive.module')
+    .then( m =>m.ReactiveModule )
+},{
+  path: 'auth',
+  loadChildren: () => import('./modules/auth/auth.module')
+    .then( m =>m.AuthModule )
+}, {
+  path: '**',
+  redirectTo: 'reactive'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
